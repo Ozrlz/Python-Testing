9e39218fb22dd6c2f2429446fbf72da75952163d
ozrlz <oscar.cucei@gmail.com>
2018-08-28 16:51:04 -0500

    [ADD] Add first endpoint to the flask app
    <4.52>

--- src/app.py
+++ src/app.py
@@ -1,1 +1,19 @@
-print ("It worked c:")
+from os import environ
+
+from flask import Flask, jsonify
+
+FLASK_PORT = environ.get('FLASK_PORT')
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_wolrd():
+    return jsonify(
+        {
+            'message': 'Hello world c:'
+        }
+    )
+
+
+if __name__ == '__main__':
+    app.run(port=FLASK_PORT, host='0.0.0.0', debug=True)
