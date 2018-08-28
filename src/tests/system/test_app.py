cff4863be60668956020ceecae7c63fe92c42968
ozrlz <oscar.cucei@gmail.com>
2018-08-28 10:42:19 -0500

    [ADD] Add tests if the menu func calls the print_blogs func and if it types an input
    <3.44>

--- src/tests/system/test_app.py
+++ src/tests/system/test_app.py
@@ -5,6 +5,18 @@ from blog import Blog
 
 class TestApp(TestCase):
 
+    def test_menu_prints_prompt(self):
+        ''' This tests if the input func was called with the that arg '''
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+    def test_menu_calls_print_blogs(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input', return_value='q'):
+                app.menu()
+                mocked_print_blogs.assert_called()
+
     def test_print_blogs(self):
         blog = Blog('Test', 'Test author')
         app.blogs = {blog.title: blog}
