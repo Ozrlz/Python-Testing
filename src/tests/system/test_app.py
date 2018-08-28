3ac8d2a71f3cab920842e30aff52d23c0bded0ef
ozrlz <oscar.cucei@gmail.com>
2018-08-28 16:26:35 -0500

    [IMP] Add other tests and an amazing decorator uwu
    <4.48>

--- src/tests/system/test_app.py
+++ src/tests/system/test_app.py
@@ -2,14 +2,38 @@ from unittest import TestCase
 from unittest.mock import patch
 import app
 from blog import Blog
+import pdb
+from functools import wraps
+import sys
+import traceback
+
+def debug_on(*exceptions):
+    if not exceptions:
+        exceptions = (AssertionError,)
+    def decorator(callback):
+        @wraps(callback)
+        def wrapper(*args, **kwargs):
+            try:
+                return callback(*args, **kwargs)
+            except exceptions:
+                info = sys.exc_info()
+                traceback.print_exception(*info)
+                pdb.post_mortem(info[2])
+        return wrapper
+    return decorator
 
 class TestApp(TestCase):
 
-    def test_menu_prints_prompt(self):
-        ''' This tests if the input func was called with the that arg '''
-        with patch('builtins.input', return_value='q') as mocked_input:
-            app.menu()
-            mocked_input.assert_called_with(app.MENU_PROMPT)
+    def setUp(self):
+        blog = Blog('Test', 'Test author')
+        app.blogs = {blog.title: blog}
+
+    def test_menu_calls_create_blog(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('c', 'Test title', 'Test author', 'q')
+            with patch('app.ask_create_blog') as mocked_ask_create_blog:
+                app.menu()
+                mocked_ask_create_blog.assert_called_once()
 
     def test_menu_calls_print_blogs(self):
         with patch('app.print_blogs') as mocked_print_blogs:
@@ -17,9 +41,28 @@ class TestApp(TestCase):
                 app.menu()
                 mocked_print_blogs.assert_called()
 
+    def test_menu_calls_ask_read_blog(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('r', 'Test', 'q')
+            with patch('app.ask_read_blog') as mocked_ask_create_blog:
+                app.menu()
+                mocked_ask_create_blog.assert_called_once()
+
+    def test_menu_calls_ask_create_post(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('p', 'Test title', 'Test author', 'q')
+            with patch('app.ask_create_post') as mocked_ask_create_blog:
+                app.menu()
+                mocked_ask_create_blog.assert_called_once()
+
+    def test_menu_prints_prompt(self):
+        ''' This tests if the input func was called with the that arg '''
+        with patch('builtins.input', return_value='q') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+
     def test_print_blogs(self):
-        blog = Blog('Test', 'Test author')
-        app.blogs = {blog.title: blog}
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('- Test by Test author (0 posts)')
@@ -43,3 +86,13 @@ class TestApp(TestCase):
                 
                 mocked_print_posts.assert_called_once()
 
+
+    @debug_on(IndexError)
+    def test_ask_create_post(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test', 'Test title', 'Test content')
+            app.ask_create_post()
+
+            self.assertEqual(app.blogs.get('Test').posts[0].title, 'Test title')
+            self.assertEqual(app.blogs.get('Test').posts[0].content, 'Test content')
+            
