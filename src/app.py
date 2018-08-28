ee2479d311e6825a46f93229e27d2235ad3c7b5e
ozrlz <oscar.cucei@gmail.com>
2018-08-28 10:41:18 -0500

    [IMP] Add an input call in the menu func

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+MENU_PROMPT = """Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit alv """
 blogs = dict() # Blog_name : Blog_object
 
 def menu():
@@ -7,6 +8,7 @@ def menu():
     # Exit :v
 
     print_blogs()
+    selection = input(MENU_PROMPT)
 
 def print_blogs():
     # Print all blogs
