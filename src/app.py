9d9e741765e3a49d115af728615fcf30804b235e
ozrlz <oscar.cucei@gmail.com>
2018-08-29 10:42:52 -0500

    [IMP] Integrate the starter_code to start the section 5
    <5.58>

--- src/app.py
+++ src/app.py
@@ -1,18 +1,31 @@
 from os import environ
 
-from flask import Flask, jsonify
+from flask import Flask
+from flask_restful import Api
 
-FLASK_PORT = environ.get('FLASK_PORT')
+from resources.item import Item, ItemList
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_wolrd():
-    return jsonify({
-            'message': 'Hello world c:'
-        }
-    )
+FLASK_PORT = environ.get('FLASK_PORT', 5000)
 
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 if __name__ == '__main__':
-    app.run(port=FLASK_PORT, host='0.0.0.0', debug=True)
+    from db import db
+
+    db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000, host='0.0.0.0', debug=True)
