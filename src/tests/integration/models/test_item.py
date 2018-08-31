70c27d5db367f47ae6e0a4932cedebb9d717eb43
ozrlz <oscar.cucei@gmail.com>
2018-08-31 15:18:58 -0500

    [ADD] Add a test to the relationship (integration)
    <6.67>

--- src/tests/integration/models/test_item.py
+++ src/tests/integration/models/test_item.py
@@ -17,3 +17,12 @@ class ItemTest(BaseTest):
             item.delete_from_db()
             self.assertIsNone(ItemModel.find_by_name('Test'))
             
+    def test_store_relationship(self):
+        with self.app_context():
+            store = StoreModel('Test store')
+            item = ItemModel('Test', 19.00, 1)
+
+            store.save_to_db()
+            item.save_to_db()
+
+            self.assertEqual(item.store.name, 'Test store')
